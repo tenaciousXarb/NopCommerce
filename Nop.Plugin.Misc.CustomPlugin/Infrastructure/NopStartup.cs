a23ba9863dee8595e02063b04facb2cc57da5181
Ahmad Arabi <65108308+tenaciousXarb@users.noreply.github.com>
2023-03-15 16:12:07 +0600

    Custom Plugin Added

---
+++ Nop.Plugin.Misc.CustomPlugin/Infrastructure/NopStartup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Infrastructure;
+using Nop.Plugin.Misc.CustomPlugin.Services;
+
+namespace Nop.Plugin.Misc.CustomPlugin.Infrastructure
+{
+    public class NopStartup : INopStartup
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICustomService, CustomService>();
+        }
+
+        public void Configure(IApplicationBuilder application)
+        {
+        }
+        public int Order => 3000;
+    }
+}
---
+++ Nop.Plugin.Misc.CustomPlugin/Services/ICustomService.cs
@@ -0,0 +1,17 @@
+﻿using Nop.Core;
+using Nop.Plugin.Misc.CustomPlugin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Misc.CustomPlugin.Services
+{
+    public interface ICustomService
+    {
+        Task<ConfigureModel> GetByIdAsync(int id);
+
+        Task UpdateModelAsync(ConfigureModel model);
+    }
+}
