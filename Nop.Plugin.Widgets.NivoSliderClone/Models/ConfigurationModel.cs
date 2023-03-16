e2cee79d4f61893468a586ca28d674a907dc7ece
Ahmad Arabi <ahmadarabi2562@gmail.com>
2023-03-16 15:07:49 +0600

    Updated the controller to store and update the metadata of the images

--- Nop.Plugin.Widgets.NivoSliderClone/Models/ConfigurationModel.cs
+++ Nop.Plugin.Widgets.NivoSliderClone/Models/ConfigurationModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Nop.Web.Framework.Mvc.ModelBinding;
 using Nop.Web.Framework.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace Nop.Plugin.Widgets.NivoSliderClone.Models
 {
@@ -9,51 +10,51 @@ namespace Nop.Plugin.Widgets.NivoSliderClone.Models
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Picture")]
         [UIHint("Picture")]
         public int Picture1Id { get; set; }
+        public IFormFile File1 { get; set; }
+        public string Picture1Url { get; set; } = string.Empty;
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Text")]
-        public string Text1 { get; set; }
+        public string? Text1 { get; set; }
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Link")]
-        public string Link1 { get; set; }
-        [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.AltText")]
-        public string AltText1 { get; set; }
+        public string? Link1 { get; set; }
 
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Picture")]
         [UIHint("Picture")]
         public int Picture2Id { get; set; }
+        public IFormFile File2 { get; set; }
+        public string Picture2Url { get; set; } = string.Empty;
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Text")]
         public string Text2 { get; set; }
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Link")]
         public string Link2 { get; set; }
-        [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.AltText")]
-        public string AltText2 { get; set; }
 
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Picture")]
         [UIHint("Picture")]
         public int Picture3Id { get; set; }
+        public IFormFile File3 { get; set; }
+        public string Picture3Url { get; set; } = string.Empty;
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Text")]
         public string Text3 { get; set; }
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Link")]
         public string Link3 { get; set; }
-        [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.AltText")]
-        public string AltText3 { get; set; }
 
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Picture")]
         [UIHint("Picture")]
         public int Picture4Id { get; set; }
+        public IFormFile File4 { get; set; }
+        public string Picture4Url { get; set; } = string.Empty;
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Text")]
         public string Text4 { get; set; }
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Link")]
         public string Link4 { get; set; }
-        [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.AltText")]
-        public string AltText4 { get; set; }
 
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Picture")]
         [UIHint("Picture")]
         public int Picture5Id { get; set; }
+        public IFormFile File5 { get; set; }
+        public string Picture5Url { get; set; } = string.Empty;
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Text")]
         public string Text5 { get; set; }
         [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.Link")]
         public string Link5 { get; set; }
-        [NopResourceDisplayName("Plugins.Widgets.NivoSliderClone.AltText")]
-        public string AltText5 { get; set; }
     }
 }
